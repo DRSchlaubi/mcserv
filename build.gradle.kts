f44e619c978897a18ac043be9324a4ecca0233e3
NyCode <nico@nycode.de>
2021-08-27 20:46:10 +0200

    Add packaging build logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    base
+}
+
+version = "1.0.0"
+
 tasks {
     val dartGenerate = register<Exec>("dartGenerate") {
         group = "dart"
@@ -23,19 +29,57 @@ tasks {
         )
     }
 
-    register<Exec>("dartBuild") {
+    val dartBuild = register<Exec>("dartBuild") {
         val fileName = if (System.getProperty("os.name").startsWith("Windows")) "mcserv.exe" else "mcserv"
         val destinationDir = project.buildDir.resolve("dart")
         doFirst {
             destinationDir.mkdirs()
         }
+        val destinationFile = destinationDir.resolve(fileName)
         group = "dart"
         commandLine = listOf(
             "dart2native",
             project.file("bin/mcserv.dart").absolutePath,
             "-o",
-            destinationDir.resolve(fileName).absolutePath
+            destinationFile.absolutePath
         )
         dependsOn(dartGenerate)
+        outputs.file(destinationFile)
+        inputs.files(project.file("lib"), project.file("i18n"), project.file("bin"))
+    }
+
+    assemble {
+        dependsOn(dartBuild)
+    }
+
+    val profiles = listOf("Release", "Debug")
+
+    for (profile in profiles) {
+        val lowerCaseProfile = profile.toLowerCase(java.util.Locale.ROOT)
+        val packageDir = project.buildDir.resolve("package").resolve(lowerCaseProfile)
+        val copyPackage = register<Copy>("copy${profile}Package") {
+            val nativeBuild = project.subprojects.first { it.name == "libmcserv" }
+                .tasks.getByName("link$profile")
+            group = "packaging"
+            from(dartBuild, project.file("libmcserv/build/lib/main/$lowerCaseProfile/liblibmcserv.so"))
+            into(packageDir)
+            dependsOn(assemble, nativeBuild)
+        }
+        val zipPackage = register<Zip>("zip${profile}Package") {
+            group = "packaging"
+            destinationDirectory.set(project.buildDir.resolve("packages"))
+            from(packageDir.absolutePath)
+            dependsOn(copyPackage)
+        }
+        val tarPackage = register<Tar>("tar${profile}Package") {
+            group = "packaging"
+            destinationDirectory.set(project.buildDir.resolve("packages"))
+            from(packageDir.absolutePath)
+            dependsOn(copyPackage)
+        }
+        register("create${profile}Package") {
+            group = "packaging"
+            dependsOn(zipPackage, tarPackage)
+        }
     }
 }
