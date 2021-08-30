ee014bc88aa133b6721f8ef8928fd0b959b18446
Michael Rittmeister <michael@rittmeister.in>
2021-08-30 17:30:53 +0200

    Update wix env var

--- packages/windows/msi/build.bat
+++ packages/windows/msi/build.bat
@@ -2,16 +2,16 @@
 
 Rem This tool generates a mcserv MSI installer file
 
-if defined WIX_HOME goto build
+if defined WIX goto build
 
 :fail
-echo "Please set WIX_HOME to point to a Wix Toolset installation"
+echo "Please set WIX to point to a Wix Toolset installation"
 exit \b 1
 
 :build
 cd McServInstaller
 mkdir build
 call dart2native ..\..\..\..\bin\mcserv.dart -o .\build\mcserv.exe
-call "%WIX_HOME%\bin\candle.exe" Product.wxs -o build\
-call "%WIX_HOME%\bin\light.exe" build\*.wixobj -o ..\mcserv.msi
+call "%WIX%\bin\candle.exe" Product.wxs -o build\
+call "%WIX%\bin\light.exe" build\*.wixobj -o ..\mcserv.msi
 del build
