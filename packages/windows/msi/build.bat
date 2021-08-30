1e215bee72227b03e3ea55cbab6905cdd3cb1422
Michael Rittmeister <michael@rittmeister.in>
2021-08-30 23:22:12 +0200

    Update PATH automatically - properly delete build folder

--- packages/windows/msi/build.bat
+++ packages/windows/msi/build.bat
@@ -9,9 +9,8 @@ echo "Please set WIX to point to a Wix Toolset installation"
 exit \b 1
 
 :build
-cd McServInstaller
-mkdir build
-call dart2native ..\..\..\..\bin\mcserv.dart -o .\build\mcserv.exe
-call "%WIX%\bin\candle.exe" Product.wxs -o build\
-call "%WIX%\bin\light.exe" build\*.wixobj -o ..\mcserv.msi
-del build
+mkdir McServInstaller\build
+call dart2native ..\..\..\bin\mcserv.dart -o .\McServInstaller\build\mcserv.exe
+call "%WIX%\bin\candle.exe" McServInstaller\Product.wxs -o .\McServInstaller\build\
+call "%WIX%\bin\light.exe" McServInstaller\build\*.wixobj -o mcserv.msi
+rmdir /Q /S .\McServInstaller\build
